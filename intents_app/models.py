eaddd079e8ffcabce230c457272a863511da1541
Sarah Trapaga <trapagasarah@gmail.com>
2019-06-08 14:53:56 -0400

    added urls and models

--- intents_app/models.py
+++ intents_app/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+class Campsite(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    campsite_type = models.CharField(max_length=100)
+    campsiteAPI_id = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name} - {self.location} - {self.campsite_type}"
+
+
+class Trip(models.Model):
+    start_date = models.DateField()
+    end_date = models.DateField()
+    campers = models.IntegerField()
+    campsite = models.ForeignKey(Campsite, on_delete=models.CASCADE, related_name='trip')
+
+    def __str__(self):
+        return self.start_date
+
+class Gear(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class ChecklistItem(models.Model):
+    camping_item = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name='checklist')
+    is_checked = models.BooleanField()
+
+    def __str__(self):
+        return self.camping_item
