581ef3e17e8a3393dc18a29a191272921c275b62
Sarah Trapaga <trapagasarah@gmail.com>
2019-06-09 11:34:13 -0400

    created react client

--- intents_project/urls.py
+++ intents_project/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.authtoken import views as auth_views
+from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('intents_app.urls')),
     path('api-auth/', auth_views.obtain_auth_token),
+    path('', views.FrontendAppView.as_view()), 
+    path('api/v1/', include('intents_app.urls')),
 ] 
