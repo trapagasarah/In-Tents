974ee6f19ca421963e4e4492d42a586dc40094d6
Sarah Trapaga <trapagasarah@gmail.com>
2019-06-10 19:07:42 -0400

    removed extra space

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,28 @@
-import React, {Component} from "react";
-import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import './App.css';
 import Gear from './components/Gear';
 import Campsites from './components/Campsites'
 
 class App extends Component {
-  
-  render() {
-      return (
-          <Router>
-              <div className="App">
 
-                  <div>
-                      <h1>In-Tents</h1>
-                      
-                  </div>
+    render() {
+        return (
+            <Router>
+                <div className="App">
 
-                  <Switch>
-                    <Route path="/gear" component={Gear}/>
-                    <Route path="/campsites" component={Campsites} />
-                  </Switch>
-              </div>
-          </Router>
-      );
-  }
+                    <div>
+                        <h1>In-Tents</h1>
+                    </div>
+
+                    <Switch>
+                        <Route path="/gear" component={Gear} />
+                        <Route path="/campsites" component={Campsites} />
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
